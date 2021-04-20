3ab39e9f0ef77db7ff4a9a80bc41bbd6e7dd4104
astroboi-lab <terry007x@korea.ac.kr>
2021-04-20 11:20:37 +0900

    multi BIG fastq files with multi barcode files

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -24,7 +24,10 @@ FASTQ = 'FASTQ/'
 # BARCD_SEQ_FILE = "barcode_seq/210316_find seq_MY_pig_PE_1st analysis.txt"  # 20210316
 BARCD_SEQ_FILE = "barcode_seq/210316_find seq_MY_pig_PE_2nd analysis_pam coedited.txt"  # 20210316
 # for multi_processing_w_brcd_arr()
-BARCD_FL_ARR = ["barcode_seq/210317_find seq_MY_1st.txt", "barcode_seq/210317_find seq_MY_2nd.txt"]  # 20210317
+BARCD_FL_ARR = [
+                "barcode_seq/3_5k_pig_nggki_find seq_MY_target and pam.txt"
+                , "barcode_seq/3_5k_pig_nggki_find seq_MY_target only.txt"
+                ]  # 20210317
 
 TOTAL_CPU = mp.cpu_count()
 MULTI_CNT = int(TOTAL_CPU*0.5)
@@ -62,12 +65,76 @@ def multi_processing():
     util.make_dict_to_excel(
         WORK_DIR + "output/result_count_" + BARCD_SEQ_FILE.replace("barcode_seq/", "").replace(".txt", ""), merge_dict)
 
+# multi BIG fastq files with multi barcode files
+def multi_processing_split_big_files_then_find_seq_from_FASTQ_w_brcd_arr():  #20210419
+    print('multi_processing_split_big_files_then_find_seq_from_FASTQ_w_brcd_arr')
+    util = Util.Utils()
+
+    brcd_arr = [
+                "barcode_seq/3_5k_pig_nggki_find seq_MY_target and pam.txt"
+                , "barcode_seq/3_5k_pig_nggki_find seq_MY_target only.txt"
+                ]  # 20210317
+
+    for brcd_path in brcd_arr:
+        brcd_list = util.read_tb_txt(WORK_DIR + brcd_path)
+        logic = Logic.Logics(brcd_list)
+
+        # fastq file name without ext
+        big_fastq_fl_nm_list = [
+            "210419_MY_pig_nggki_3_5"
+        ]
+        fastq_ext = '.fastq'
+
+        for fastq_fl_nm in big_fastq_fl_nm_list:
+            # split big file
+            split_init = {'big_file_path': WORK_DIR + FASTQ + fastq_fl_nm + fastq_ext
+                                , 'num_row': 4000000
+                                , 'splited_files_dir': WORK_DIR + FASTQ + fastq_fl_nm + "/"
+                                , 'output_file_nm': fastq_fl_nm
+                                , 'output_file_ext': fastq_ext
+
+            }
+            util.split_big_file_by_row(split_init)
+
+            # get splited_files path
+            sources = util.get_files_from_dir(split_init['splited_files_dir'] + '*.fastq')
+
+            result_dict = {}
+            for splited_fastq_fl in sources:
+                print("get_FASTQ_seq_list :", splited_fastq_fl)
+                fastq_list = util.get_FASTQ_seq_list(splited_fastq_fl)
+
+                # divide data_list by MULTI_CNT
+                splited_fastq_list = np.array_split(fastq_list, MULTI_CNT)
+                fastq_list.clear()
+
+                print("platform.system() : ", SYSTEM_NM)
+                print("total cpu_count : ", str(TOTAL_CPU))
+                print("will use : ", str(MULTI_CNT))
+                pool = mp.Pool(processes=MULTI_CNT)
+                ## analyze FASTQ seq after barcode seq
+                pool_list = pool.map(logic.get_dict_multi_p_seq_from_FASTQ, splited_fastq_list)
+                ## analyze whole FASTQ seq
+                # pool_list = pool.map(logic.get_dict_multi_p_seq_from_whole_FASTQ, splited_fastq_list)
+
+                print("merge_pool_list_to_result_dict")
+                logic.merge_pool_list_to_result_dict(pool_list, result_dict)
+                pool.close()
+                pool_list[:] = []
+
+            print("make excel file")
+            util.make_dict_to_excel(
+                WORK_DIR + "output/result_" + fastq_fl_nm + "_" + brcd_path.replace("barcode_seq/", "").replace(".txt",
+                                                                                                                     ""),
+                result_dict)
+            result_dict.clear()
+
 
 def multi_processing_w_brcd_arr():  # 20210317
     util = Util.Utils()
     fastq_fl_arr = [
-                    "503_707.fastq"
-                    , "504_708.fastq"
+                    "pig35k_ngpe_plasmid_1.fastq"
+                    , "pig35k_ngpe_plasmid_2.fastq"
                     ]
 
     for brcd_fl in BARCD_FL_ARR:
@@ -230,4 +297,5 @@ if __name__ == '__main__':
     # multi_processing_w_solo_fastq()
     # multi_processing_split_big_files_then_find_seq_from_FASTQ()
     multi_processing_w_brcd_arr()
+    # multi_processing_split_big_files_then_find_seq_from_FASTQ_w_brcd_arr()
     print("::::::::::: %.2f seconds ::::::::::::::" % (time.perf_counter() - start_time))
