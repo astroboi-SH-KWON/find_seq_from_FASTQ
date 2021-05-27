50c0f2fd4fb5d22756e5bc9e72305a00d674596c
astroboi-lab <terry007x@korea.ac.kr>
2021-05-27 09:21:21 +0900

    add brcd_pos_arr, brcd_len, rev_com_flag

--- MultiProcessing_dict.py
+++ MultiProcessing_dict.py
@@ -19,11 +19,12 @@ else:
     # WORK_DIR = "D:/000_WORK/JangHyeWon_ShinJeongHong/20200604/WORK_DIR/"
     WORK_DIR = "D:/000_WORK/LeeMinYung/20210316/WORK_DIR/"  # 20210316
 
-FASTQ = '/FASTQ/'
+FASTQ = '/FASTQ/36k_hw_PE2_210524/Sample_36K_in_vivo_PE2_TF/'
 BRCD = '/barcode_seq/'
 # for multi_processing_w_brcd_arr()
 BARCD_FL_ARR = [
-                "19k_EB.txt"
+                # "EB_BE_findseq.txt"
+                "PE_vivo_lib_input file_200609_final.txt"
                 # , "2104224.txt"
                 # , "2104222.txt"
                 # , "2104223.txt"
@@ -33,17 +34,26 @@ BARCD_FL_ARR = [
                 ]
 
 # fastq file name without ext
-FQ_EXT = '.fastq'
+# FQ_EXT = '.fastq'
+FQ_EXT = '.fq'
 FASTG_ARR = [
-            "19k_ramu"
-             , "19k_my"
+            # "ABEng_1"
+            # "36K_in_vivo_PE2_TF_Rep1_D501_D705_1"
+            #  , "36K_in_vivo_PE2_TF_Rep1_D501_D705_2"
+             "36K_in_vivo_PE2_TF_Rep1_D502_D706_1"
+             , "36K_in_vivo_PE2_TF_Rep1_D502_D706_2"
+             , "36K_in_vivo_PE2_TF_Rep2_D503_D705_1"
+             , "36K_in_vivo_PE2_TF_Rep2_D503_D705_2"
+             , "36K_in_vivo_PE2_TF_Rep2_D504_D706_1"
+             , "36K_in_vivo_PE2_TF_Rep2_D504_D706_2"
+            #  , "ABEngg"
+            #  , "ABEng"
             ]
 
 BRCD_POS_ARR_fr_FQ = [0, 0]
-BRCD_LEN = 19
 
 TOTAL_CPU = mp.cpu_count()
-MULTI_CNT = int(TOTAL_CPU*0.8)
+MULTI_CNT = int(TOTAL_CPU*0.7)
 
 ############### end setting env ################
 
@@ -59,14 +69,19 @@ def multi_processing_dict_split_big_files_then_find_seq_from_FASTQ_w_brcd_arr():
         brcd_dict = logic_prep.list_to_dict_by_ele_key(brcd_list, 1)
         logic = Logic.Logics(brcd_dict)
 
+        ### st set logic init ####################################
         # set bar_code estimated start and end position from fastq, default : [0, 0]
         logic.brcd_pos_arr = BRCD_POS_ARR_fr_FQ
         # set bar_code length, default : 20
-        logic.brcd_len = BRCD_LEN
+        logic.brcd_len = logic_prep.check_brcd_length(brcd_dict, brcd_path)
+        print("BRCD_len :", logic.brcd_len)
+        # set reversed complement flag
+        logic.rev_com_flag = True
+        ### en set logic init ####################################
 
         # BIG fastq files
         for fastq_fl_nm in FASTG_ARR:
-            # split big file
+            # st split_big_file_by_row
             split_init = {'big_file_path': WORK_DIR + FASTQ + fastq_fl_nm + FQ_EXT
                                 , 'num_row': 4000000
                                 , 'splited_files_dir': WORK_DIR + FASTQ + fastq_fl_nm + "/"
@@ -74,14 +89,18 @@ def multi_processing_dict_split_big_files_then_find_seq_from_FASTQ_w_brcd_arr():
                                 , 'output_file_ext': FQ_EXT
 
             }
-            # util.split_big_file_by_row(split_init)
+            util.split_big_file_by_row(split_init)
+            # # DONE split_big_file_by_row
 
             # get splited_files path
-            sources = util.get_files_from_dir(split_init['splited_files_dir'] + '*.fastq')
+            sources = util.get_files_from_dir(split_init['splited_files_dir'] + '*' + FQ_EXT)
+            # check sources' length
+            if len(sources) == 0:
+                print("[ERROR] : check *." + FQ_EXT, "files path, it's empty")
+                exit()
 
             result_dict = {}
             for splited_fastq_fl in sources:
-                print("get_FASTQ_seq_list :", splited_fastq_fl)
                 fastq_list = util.get_FASTQ_seq_list(splited_fastq_fl)
 
                 # divide data_list by MULTI_CNT
@@ -120,12 +139,18 @@ def multi_processing_dict_w_brcd_arr():
         brcd_dict = logic_prep.list_to_dict_by_ele_key(brcd_list, 1)
         logic = Logic.Logics(brcd_dict)
 
-        # set bar_code estimated start and end position from fastq, default : [0, 0]
+        ### st set logic init ####################################
+        # # set bar_code estimated start and end position from fastq, default : [0, 0]
         logic.brcd_pos_arr = BRCD_POS_ARR_fr_FQ
-        # set bar_code length, default : 20
-        logic.brcd_len = BRCD_LEN
+        # # set bar_code length, default : 20
+        logic.brcd_len = logic_prep.check_brcd_length(brcd_dict, brcd_fl)
+        print("BRCD_len :", logic.brcd_len)
+        # # set reversed complement flag
+        # logic.rev_com_flag = True
+        ### en set logic init ####################################
 
         for fastq_fl in FASTG_ARR:
+            result_dict = {}
             print("get_FASTQ_seq_list : ", fastq_fl)
             fastq_list = util.get_FASTQ_seq_list(WORK_DIR + FASTQ + fastq_fl + FQ_EXT)
 
@@ -138,30 +163,28 @@ def multi_processing_dict_w_brcd_arr():
             ## analyze FASTQ seq after barcode seq
             pool_list = pool.map(logic.get_dict_by_brcd_w_brcd_range_from_FASTQ, splited_fastq_list)
 
-            merge_dict = logic.merge_pool_list(pool_list)
+            print("merge_pool_list_to_result_dict")
+            logic.merge_pool_list_to_result_dict(pool_list, result_dict)
             pool.close()
             pool_list[:] = []
 
-            logic_prep.add_missing_brcd_to_dict(brcd_list, merge_dict)
+            logic_prep.add_missing_brcd_to_dict(brcd_list, result_dict)
             util.make_dict_to_excel(WORK_DIR + "output/result_count_" + fastq_fl + "_" + brcd_fl.replace(".txt", ""),
-                                    merge_dict)
-
+                                    result_dict)
 
 def split_big_file():
     print('st split_big_file')
-    fastq_fl_nm = 'Kcnq4 p.L47P'
-    fastq_ext = '.fastq'
-
     util = Util.Utils()
-    # split big file
-    split_init = {'big_file_path': WORK_DIR + FASTQ + fastq_fl_nm + fastq_ext
-        , 'num_row': 1000000  # 4000000
-        , 'splited_files_dir': WORK_DIR + FASTQ + fastq_fl_nm + "/"
-        , 'output_file_nm': fastq_fl_nm
-        , 'output_file_ext': fastq_ext
+    for fastq_fl_nm in FASTG_ARR:
+        # split big file
+        split_init = {'big_file_path': WORK_DIR + FASTQ + fastq_fl_nm + FQ_EXT
+            , 'num_row': 4000000
+            , 'splited_files_dir': WORK_DIR + FASTQ + fastq_fl_nm + "/"
+            , 'output_file_nm': fastq_fl_nm
+            , 'output_file_ext': FQ_EXT
 
-                  }
-    util.split_big_file_by_row(split_init)
+                      }
+        util.split_big_file_by_row(split_init)
     print('DONE split_big_file')
 
 
